3204a1aae3ca577a27d40d8e88e46f760a8b8cd7
Aleph-Phoebe <phbwangui47@gmail.com>
2024-06-30 07:36:28 +0300

    COMPLETED

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+
+// Display currentUser
+const currentUser = 'Aleph Phoebe';
+
+// Display welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Display excitedWelcomeMessage
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Display shortGreeting
+const firstInitial = currentUser[0];
+const shortGreeting = `Welcome, ${firstInitial}!`;
+
